3b030b5b4bb31e020102407c72e268ae103eca53
Isara Rungvitayakul <isaru_66@hotmail.com>
2025-05-27 00:15:56 +0700

    update firewall policy

--- hub-firewall.tf
+++ hub-firewall.tf
@@ -20,7 +20,10 @@ resource "azurerm_firewall" "azfirewall" {
     subnet_id            = azurerm_subnet.hub-subnet-2.id
     public_ip_address_id = azurerm_public_ip.azfirewall.id
   }
+
+  firewall_policy_id = azurerm_firewall_policy.azfirewall_policy.id
 }
+
 resource "azurerm_monitor_diagnostic_setting" "azfirewall" {
   name                       = "azfirewall-diagnostics"
   target_resource_id         = azurerm_firewall.azfirewall.id
@@ -56,19 +59,72 @@ resource "azurerm_firewall_policy_rule_collection_group" "example" {
   application_rule_collection {
     name     = "allow-apim-to-blob-dsl"
     priority = 500
-    action   = "Deny"
+    action   = "Allow"
     rule {
-      name = "app_rule_collection1_rule1"
-      protocols {
-        type = "Http"
-        port = 80
-      }
+      name = "apim-to-blob-dsl"
       protocols {
         type = "Https"
         port = 443
       }
       source_addresses  = ["10.100.2.0/24"]
       destination_fqdns = ["apigblstorageprdsg1.blob.core.windows.net"]
+    }
+    rule {
+      name        = "Global Rule"
+      description = "Allow access to Microsoft.com"
+      protocols {
+        type = "Https"
+        port = 443
+      }
+      source_addresses  = ["10.100.2.0/24"]
+      destination_fqdns = ["*.microsoft.com"]
+    }
+    //
+    
+    rule {
+      name        = "APIM to Azure Monitor"
+      description = "Allow access to azure monitor"
+      protocols {
+        type = "Https"
+        port = 443
+      }
+      source_addresses  = ["10.100.2.0/24"]
+      destination_fqdns = ["gcs.prod.warm.ingestion.monitoring.azure.com"]
+    }
+  }
+
+  
+  network_rule_collection {
+    name     = "Allow-APIM"
+    action   = "Allow"
+    priority = 3000
+    rule {
+      name                  = "allow-keyvault"
+      protocols             = ["TCP"]
+      source_addresses      = ["10.100.2.0/24"]
+      destination_ports     = ["443"]
+      destination_addresses = ["AzureKeyVault"]
+    }
+    rule {
+      name                  = "allow-storage"
+      protocols             = ["TCP"]
+      source_addresses      = ["10.100.2.0/24"]
+      destination_ports     = ["443"]
+      destination_addresses = ["Storage"]
+    }
+    rule {
+      name                  = "allow-sql"
+      protocols             = ["TCP"]
+      source_addresses      = ["10.100.2.0/24"]
+      destination_ports     = ["1433"]
+      destination_addresses = ["Sql"]
+    }
+    rule {
+      name                  = "allow-apim-internal-debug"
+      protocols             = ["TCP"]
+      source_addresses      = ["10.100.2.0/24"]
+      destination_ports     = ["12000"]
+      destination_addresses = ["AzureMonitor"]
     }
   }
 
